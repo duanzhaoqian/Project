7605c13d47099d972f32fb53290e25dde643565d
duanzq <duanzhaoqian@outlook.com>
2015-04-04 14:51:14 +0800

    Test String C# Java

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private String str = "good";
+        private char[] cha = { 'a', 'b', 'c' };
+        private int i = 10;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.change(program.str, program.cha,program.i);
+            Console.WriteLine(program.str + " and " + program.cha);
+            Console.ReadKey();
+        }
+
+        void change(String str, char[] ch,int i)
+        {
+            string.Intern(str);
+            str = "test ok";
+            ch[0] = 'g';
+            i = 100;
+        }
+    }
+}
