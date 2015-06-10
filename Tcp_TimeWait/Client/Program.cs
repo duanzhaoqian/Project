4bc4c25160164976257bc142456d3cd55ecee9e0
duanzq <duanzhaoqian@outlook.com>
2015-06-10 16:10:30 +0800

    TIME_WAIT

---
+++ Tcp_TimeWait/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpClient.Connect("192.168.3.68", 8888);
+            byte[] bytes = Encoding.UTF8.GetBytes("C#");
+            //Thread thread=new Thread(() =>
+            //{
+            //    while (true)
+            //    {
+            //        int n = 0;
+            //        byte[] bytesbuff = new byte[10];
+            //        StringBuilder stringBuilder = new StringBuilder();
+            //        while (n > -1)
+            //        {
+            //            n = tcpClient.Receive(bytesbuff, 0, bytesbuff.Length, SocketFlags.None);
+            //            stringBuilder.Append(Encoding.UTF8.GetString(bytesbuff, 0, n));
+            //        }
+            //        Console.WriteLine(stringBuilder);
+            //    }
+            //});
+            //thread.IsBackground = true;
+            //thread.Start();
+            tcpClient.Send(bytes, 0, bytes.Length, SocketFlags.None);
+            //  tcpClient.Close();
+            // Console.ReadKey();
+            //tcpClient.BeginSend(bytes, 0, bytes.Length, 0, (c) => tcpClient.EndSend(c), null);
+
+            //  tcpClient.Client.BeginSend(Encoding.UTF8.GetBytes("C#"),)
+
+            //  tcpClient.LingerState.Enabled = false;
+            // byte[] bytes = new byte[10];
+            // IList<ArraySegment<byte>> list = new List<ArraySegment<byte>>();
+            // tcpClient.Client.Send(Encoding.UTF8.GetBytes("C#"));
+            //     int n = 0;
+            //while (n > -1)
+            //{
+            //    n = tcpClient.Client.Receive(list);
+            //}
+
+            //tcpClient.Shutdown(SocketShutdown.Both);
+            //tcpClient.Dispose();
+            //  tcpClient.Close();
+
+            Thread.Sleep(10 * 1000);
+            tcpClient.Close();
+            Console.WriteLine("Close");
+            Thread.Sleep(100 * 1000);
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ Tcp_TimeWait/Server/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 8888));
+            socket.Listen(10);
+
+            Thread thread1 = new Thread(() =>
+            {
+                while (true)
+                {
+                    Socket socket1 = null;
+                    //try
+                    //{
+                    socket1 = socket.Accept();
+                    Thread thread = new Thread(() =>
+                    {
+                        Socket socket2 = socket1;
+                        //try
+                        //{
+                        byte[] bytes = new byte[10];
+                        //int n = 0;
+                        while (true)
+                        {
+                            try
+                            {
+                                int n = socket2.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+                                string str = Encoding.UTF8.GetString(bytes, 0, n);
+                                if (!string.IsNullOrEmpty(str))
+                                {
+                                    Console.WriteLine(str);
+                                }
+
+                                socket2.Send(Encoding.UTF8.GetBytes("Server " + str));
+                            }
+                            catch (Exception exception)
+                            {
+                                socket2.Close();
+                                Console.WriteLine("Receive");
+                                //  Console.WriteLine(exception);
+                                break;
+                            }
+
+                        }
+                        //while (true)
+                        //{
+                        //    Thread.Sleep(10000);
+                        //}
+                        //}
+                        //catch (Exception ex)
+                        //{
+                        //    Console.WriteLine("socket2");
+                        //}
+                        //finally
+                        //{
+                        //    socket2.Close();
+                        //}
+                    });
+                    thread.IsBackground = true;
+                    thread.Start();
+
+                    //}
+                    //catch (Exception exception)
+                    //{
+                    //    Console.WriteLine("socket1");
+                    //}
+                    //finally
+                    //{
+                    //    if (socket1 != null) socket1.Close();
+                    //}
+                }
+
+            });
+            thread1.IsBackground = true;
+            thread1.Start();
+            Console.ReadKey();
+        }
+    }
+}
